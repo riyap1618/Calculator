c4f6eca820ab5c187841ddaef2ef83f34d19f18c
Riya Patel <rpp28@cam.ac.uk>
2022-07-03 15:10:13 +0100

    With if...else... statements

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ const readline = require('readline-sync');
 
 console.log('Welcome to the calculator!');
 
+console.log('Please enter the operator:');
+const operator = readline.prompt();
+
 console.log('Please enter the first number:');
 const response1 = readline.prompt();
+const firstNum = +response1;
 
 console.log('Please enter the second number:');
 const response2 = readline.prompt();
+const secondNum = +response2;
 
-console.log('The product is ' + response1*response2 + '.');
+let ans = 0;
+if (operator === '*') {
+    ans = firstNum * secondNum;
+}
+else if (operator === '+') {
+    ans = firstNum + secondNum;
+}
+else if (operator === '-') {
+    ans = firstNum - secondNum;
+}
+else if (operator === '/') {
+    ans = firstNum / secondNum;
+}
+
+console.log('The answer is ' + ans + '.');
